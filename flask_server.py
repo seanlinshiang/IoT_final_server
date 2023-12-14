1bf31308a63e22d5e7f304d3af051ee4728b244f
Sean Lin <seanlinshiang@gmail.com>
2023-12-14 14:45:44 +0800

    new version

--- flask_server.py
+++ flask_server.py
@@ -6,18 +6,38 @@ from flask_cors import CORS
 import time
 
 MAX_HISTORY_LEN = 10
+MAX_DISTANCE = 300
 
+class CountAverage:
+    def __init__(self):
+        self.size = 0
+        self.average = 0
+
+    def add(self, val):
+        self.average = (self.size * self.average + val) / (self.size + 1)
+        self.size += 1
+
+    def get_ave(self):
+        return self.average
+    
+    def get_accum(self):
+        return self.average * self.size
+    
 product_serial = Serial('/dev/ttyACM0', 9600, timeout=.5)
 aisle_serial = Serial('/dev/ttyACM1', 9600, timeout=.5)
 product_distance = 0
 aisle_distance = 0
 product_num = 0
-
-
+last_buy_time = time.time()
+sold_num = 0
+average_stop_time = CountAverage()
+average_purchase_time = CountAverage()
 
 app = Flask(__name__)
 CORS(app)
 
+
+    
 def read_distance(serial):
     if not serial.inWaiting():
         return None
@@ -37,7 +57,7 @@ def read_distance(serial):
     return distance
 
 def read_product_serial():
-    global product_distance, product_num
+    global product_distance, product_num, last_buy_time, sold_num
     product_num = 3
     min_error = 40
     prev_product_distance = None
@@ -47,28 +67,29 @@ def read_product_serial():
         if dist is None:
             continue
         product_distance = dist
-        print("product sensor distance:", product_distance)
+        # print("product sensor distance:", product_distance)
 
         if prev_product_distance is None:
             prev_product_distance = product_distance
             continue
         
-        taken = product_distance > prev_product_distance + min_error
+        buy = product_distance > prev_product_distance + min_error
         refill = product_distance < prev_product_distance - min_error
-        if taken:
-            product_num -= 1
+        if buy:
+            sold_num += 1
             prev_product_distance = product_distance
-            print('taken:', product_num)
+            last_buy_time = time.time()
+            print('buy:', sold_num)
         elif refill:
-            product_num += 1
             prev_product_distance = product_distance
-            print('refill:', product_num)
+            print('refill..')
         
 
 def read_aisle_serial():
-    global aisle_distance
+    global aisle_distance, last_buy_time, average_purchase_time, average_stop_time
+    block_criteria = 100
     prev_aisle_distance = None
-    min_error = 100
+    min_error = 40
     block = False
     enter_time = 0
     leave_time = 0
@@ -77,36 +98,51 @@ def read_aisle_serial():
         if dist is None:
             continue
         aisle_distance = dist
-        print("aisle sensor distance:", aisle_distance)
+        # print("aisle sensor distance:", aisle_distance)
         
         if prev_aisle_distance is None:
             prev_aisle_distance = aisle_distance
             continue
         
-        enter = not block and aisle_distance < prev_aisle_distance - min_error
-        leave = block and aisle_distance > prev_aisle_distance + min_error
+        enter = not block and aisle_distance < block_criteria and prev_aisle_distance > block_criteria
+        leave = block and aisle_distance > block_criteria and prev_aisle_distance < block_criteria
         
         if enter:
+            print('enter')
             block = True
             enter_time = time.time()
-            print('enter')
-            prev_aisle_distance = aisle_distance
         elif leave:
+            print('leave')
             block = False
             leave_time = time.time()
-            time_diff = leave_time - enter_time
-            print(f'time_dif:{time_diff:.2f}')
-            print('leave')
-            prev_aisle_distance = aisle_distance
+            stop_time = leave_time - enter_time
+            average_stop_time.add(stop_time)
+            print(f'time_diff:{stop_time:.2f}')
 
-@app.route('/storage', methods=['GET'])
-def storage():
-    global product_distance
-    return jsonify(message=product_distance)
+            if last_buy_time > enter_time:
+                enter_to_buy_time = last_buy_time - enter_time
+                print(f'enter to buy:{enter_to_buy_time:.2f}')
+                average_purchase_time.add(enter_to_buy_time)
+
+        prev_aisle_distance = aisle_distance
+@app.route('/get_data', methods=['GET'])
+def get_data():
+    global product_distance, average_stop_time, average_purchase_time, sold_num
+    storage = (1 - (product_distance / MAX_DISTANCE))*100
+    storage = 0 if storage < 0 else storage
+    storage = 100 if storage > 95 else storage 
+    response = {
+        "storage": storage,
+        "average_stop_time": average_stop_time.get_ave(),
+        "average_purchase_time": average_purchase_time.get_ave(),
+        "accum_stop_time": average_stop_time.get_accum(),
+        "sold_num": sold_num,
+    }
+    return jsonify(response)
 
 
 def start_app():
-    app.run(debug=True, use_reloader=False, host="0.0.0.0", threaded=True)
+    app.run(debug=False, use_reloader=False, host="0.0.0.0", threaded=True)
 
 if __name__ == '__main__':
     # start_app()
